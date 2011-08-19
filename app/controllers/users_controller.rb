6c3e9a8280711a36b61f6679a34fa3ae3f7d2fce
Francisco Arevalo <farevalod@gmail.com>
2011-08-19 00:10:55 -0400

    Usuarios y sign-in completos y validados.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,18 @@
 class UsersController < ApplicationController
   def new
+  	@user = User.new
   	@title = "Sign up"
+  end
+
+  def create
+  	@user = User.new(params[:user])
+	if @user.save
+		flash[:success] = "Bienvenido a la plataforma ElClick!"
+		redirect_to @user
+	else
+		@title = "Sign up"
+		render 'new'
+	end
   end
 
   def show
